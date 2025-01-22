c549c183333d4ed08e76b2a6c5c277d0ad309adf
Tizio a caso <gullottidiego2@gmail.com>
2025-01-22 01:05:05 +0100

    UI
    Ho aggiunto la yoi principale è il bottone per aggiungere le note, le note sono un oggetto a parte quando si clicca sul bottone per aggiungere la nota si aggiunge nella UI principale

---
+++ NoteApp/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Note;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace NoteApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SendInput(object sender, RoutedEventArgs e)
+        {
+            string? titleInput = TitleInput.Text;
+            string? contentInput = TitleInput.Text;
+
+            if (string.IsNullOrWhiteSpace(titleInput))
+            {
+                MessageBox.Show("Inserisci del testo!", "Errore", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            AddNote(titleInput, contentInput);
+
+            ClosePanel(sender, e);
+        }
+
+        private void AddNote(string titleInput, string contentInput)
+        {
+            NotePanel notePanel = new()
+            {
+                Title = titleInput,
+                Content = contentInput
+            };
+
+            NoteList.Children.Add(notePanel);
+
+            System.Diagnostics.Debug.WriteLine($"Aggiunto bottone: {notePanel.Title}");
+        }
+
+        private void OpenPanel(object sender, RoutedEventArgs e)
+        {
+            // Mostra il pannello
+            OverlayPanel.Visibility = Visibility.Visible;
+
+            // Esegui animazione
+            var storyboard = (Storyboard)FindResource("ShowPanelAnimation");
+            storyboard.Begin();
+        }
+
+        private void ClosePanel(object sender, RoutedEventArgs e)
+        {
+            // Esegui animazione
+            var storyboard = (Storyboard)FindResource("HidePanelAnimation");
+            storyboard.Completed += (s, ev) =>
+            {
+                // Nasconde il pannello solo dopo l'animazione
+                OverlayPanel.Visibility = Visibility.Collapsed;
+            };
+            storyboard.Begin();
+            TitleInput.Text = string.Empty;
+            ContentInput.Text = string.Empty;
+        }
+    }
+}
---
+++ NoteApp/Note.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Note
+{
+    public partial class NotePanel : UserControl
+    {
+        public required string Title { get; set; }
+
+        public required string Content { get; set; }
+
+        public NotePanel()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void OpenNote(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Nota: {Title}");
+        }
+
+        private void ModifyNote(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Modifica Nota: {Title}");
+        }
+
+        private void DeleteNote(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Elimina Nota: {Title}");
+        }
+
+
+        //private void OpenNote(object sender, RoutedEventArgs e)
+        //{
+        //    Button? clickedButton = sender as Button;
+        //    MessageBox.Show($"Hai cliccato {clickedButton!.Content}");
+        //}
+
+        //private void ModifyPanel(object sender, RoutedEventArgs e)
+        //{
+        //    if (NoteList.Children.Count > 0)
+        //    {
+        //        Border panelBorder = (Border)NoteList.Children[^1];
+        //        Grid panelGrid = (Grid)panelBorder.Child;
+        //        Button NoteButton = (Button)panelGrid.Children[0];
+        //        NoteButton.Content = "Nota modificata";
+        //    }
+        //}
+
+        //private void RemovePanel(object sender, RoutedEventArgs e)
+        //{
+        //    if (NoteList.Children.Count > 0)
+        //    {
+        //        NoteList.Children.RemoveAt(NoteList.Children.Count - 1);
+        //    }
+        //}
+    }
+}
